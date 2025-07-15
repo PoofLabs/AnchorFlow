dc741772f30120deb46fa2585a0cfe2e4787b6cc
Patt <56939149+pswilker@users.noreply.github.com>
2025-07-14 21:03:51 -0600

    Create walletprovider.tsx

---
+++ src/components/wallet/walletprovider.tsx
@@ -0,0 +1,44 @@
+
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Import default styles
+import '@solana/wallet-adapter-react-ui/styles.css';
+import './WalletStyles.css';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const SolanaWalletProvider = ({ children }: Props) => {
+  // You can also provide a custom RPC endpoint
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
